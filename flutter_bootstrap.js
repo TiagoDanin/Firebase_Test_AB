d31601b876362d80384e12a5d2bab09ea3cf18d0
Tiago Danin <TiagoDanin@outlook.com>
2024-12-12 21:30:11 -0300

    chore: update web build
    Updated the web build with new assets and configurations.
    - Updated service worker version.
    - Changed base href to '/Firebase
    _Test_AB/'.

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"edd8546116457bdf1c5bdfb13ecb9463d2bb5e
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "456074590"
+    serviceWorkerVersion: "1725335170"
   }
 });
